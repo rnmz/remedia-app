c89c0a35d4705e70ede9055b673e1a6c5ef867da
rnmz <naomiruno4@gmail.com>
2025-04-07 20:12:27 +0300

    add documentation to annotations

--- core/src/main/java/dev/runo/core/network/annotation/OptionalApiKey.kt
+++ core/src/main/java/dev/runo/core/network/annotation/OptionalApiKey.kt
@@ -1,5 +1,9 @@
 package dev.runo.core.network.annotation
 
+/**
+ * Indicates that the API key will be included if it is provided.
+ * If no API key is available, the operation will proceed without it.
+ */
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
-annotation class OptionalApiKey()
+annotation class OptionalApiKey
--- core/src/main/java/dev/runo/core/network/annotation/RequiresApiKey.kt
+++ core/src/main/java/dev/runo/core/network/annotation/RequiresApiKey.kt
@@ -1,5 +1,9 @@
 package dev.runo.core.network.annotation
 
+/**
+ * Indicates that the operation will only be executed if a valid API key is provided.
+ * If no API key is available, the operation will not be executed.
+ */
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
 annotation class RequiresApiKey
