a85649d0b0fe438609c976e3aaecd0aca5926dfe
rnmz <naomiruno4@gmail.com>
2025-04-07 18:49:00 +0300

    remove Loading status from WorkStatus.kt

--- core/src/main/java/dev/runo/core/common/WorkStatus.kt
+++ core/src/main/java/dev/runo/core/common/WorkStatus.kt
@@ -3,5 +3,4 @@ package dev.runo.core.common
 sealed interface WorkStatus<out T> {
     data class Success<out T>(val data: T): WorkStatus<T>
     data class Error(val error: AppError): WorkStatus<Nothing>
-    data object Loading: WorkStatus<Nothing>
 }
