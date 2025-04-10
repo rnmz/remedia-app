a7da38be10c8e3fcc6a482a8ba2bd4bc22ccbc18
rnmz <naomiruno4@gmail.com>
2025-04-10 19:19:06 +0300

    update log tag

--- core/src/main/java/dev/runo/core/network/AuthInterceptor.kt
+++ core/src/main/java/dev/runo/core/network/AuthInterceptor.kt
@@ -2,7 +2,7 @@ package dev.runo.core.network
 
 import android.util.Log
 import dev.runo.core.DataStoreManager
-import dev.runo.core.common.Constants
+import dev.runo.core.common.NETWORK_LOG_TAG
 import dev.runo.core.network.annotation.OptionalApiKey
 import dev.runo.core.network.annotation.RequiresApiKey
 import okhttp3.Interceptor
@@ -31,7 +31,7 @@ class AuthInterceptor @Inject constructor(
             optionalApiKey != null -> handleOptionalApiKey(request, apiKey, method, endpoint, chain)
             requiresApiKey != null -> handleRequiredApiKey(request, apiKey, method, endpoint, chain)
             else -> {
-                Log.d(Constants.LOG_TAG, "[$method] $endpoint does not requiring an API key.")
+                Log.d(NETWORK_LOG_TAG, "[$method] $endpoint does not requiring an API key.")
                 chain.proceed(request)
             }
         }
@@ -45,14 +45,14 @@ class AuthInterceptor @Inject constructor(
         chain: Interceptor.Chain
     ): Response {
         return if (apiKey.isNotBlank()) {
-                Log.d(Constants.LOG_TAG, "[$method] $endpoint was executed successfully with an API key.")
+                Log.d(NETWORK_LOG_TAG, "[$method] $endpoint was executed successfully with an API key.")
                 val newRequest = request.newBuilder()
                     .addHeader("Authorization", "Bearer $apiKey")
                     .build()
                 chain.proceed(newRequest)
             }
             else {
-                Log.d(Constants.LOG_TAG, "[$method] $endpoint was executed successfully without an API key.")
+                Log.d(NETWORK_LOG_TAG, "[$method] $endpoint was executed successfully without an API key.")
                 chain.proceed(request)
             }
     }
@@ -65,14 +65,14 @@ class AuthInterceptor @Inject constructor(
         chain: Interceptor.Chain
     ): Response {
         return if (apiKey.isNotBlank()) {
-            Log.d(Constants.LOG_TAG, "[$method] $endpoint required an API key and was executed successfully.")
+            Log.d(NETWORK_LOG_TAG, "[$method] $endpoint required an API key and was executed successfully.")
             val newRequest = request.newBuilder()
                 .addHeader("Authorization", "Bearer $apiKey")
                 .build()
             chain.proceed(newRequest)
         }
         else {
-            Log.w(Constants.LOG_TAG, "[$method] $endpoint not executed: API key is required but was null or empty.")
+            Log.w(NETWORK_LOG_TAG, "[$method] $endpoint not executed: API key is required but was null or empty.")
             Response.Builder()
                 .code(401) // HTTP 401 - Unauthorized
                 .message("Missing API key")
