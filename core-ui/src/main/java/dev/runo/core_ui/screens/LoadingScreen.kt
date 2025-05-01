600418413ad0410489d204518ce961021ead50b1
rnmz <naomiruno4@gmail.com>
2025-05-01 22:05:59 +0300

    loading screen

---
+++ core-ui/src/main/java/dev/runo/core_ui/screens/LoadingScreen.kt
@@ -0,0 +1,26 @@
+package dev.runo.core_ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingScreen(text: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.size(50.dp)
+        )
+        Text(text)
+    }
+}
