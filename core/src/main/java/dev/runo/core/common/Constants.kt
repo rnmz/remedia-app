280f773d4b5695928fee3aa19a3d6519e2943218
rnmz <naomiruno4@gmail.com>
2025-05-23 21:39:45 +0300

    update constants

--- core/src/main/java/dev/runo/core/common/Constants.kt
+++ core/src/main/java/dev/runo/core/common/Constants.kt
@@ -3,4 +3,10 @@ package dev.runo.core.common
 const val RELEASE_API_DOMAIN = ""
 const val DEBUG_API_DOMAIN = ""
 
+const val RELEASE_AUTH_DOMAIN = ""
+const val DEBUG_AUTH_DOMAIN = ""
+
+const val RELEASE_CDN_DOMAIN = ""
+const val DEBUG_CDN_DOMAIN = ""
+
 const val NETWORK_LOG_TAG = "dev.runo.RemediaApplication.Network"
